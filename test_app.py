d3aa5e6037d24d4db83becde9328f5a37dd24280
EloimBorges <Borges.eloim@gmail.com>
2026-03-18 23:12:24 -0400

    Added exploratory test cases

--- test_app.py
+++ test_app.py
@@ -49,3 +49,27 @@ def test_tiempo_real():
 def test_tiempo_sistema():
     r = autenticar_usuario("admin", "1234")
     assert r["response_time_ms"] < 500
+
+
+    # -----------Pruebas Exploratorias-------------------
+
+def test_exploratoria_usuario_con_espacios():
+    r = autenticar_usuario(" admin ", "1234")
+    assert r["success"] == False
+
+def test_exploratoria_usuario_en_mayusculas():
+    r = autenticar_usuario("ADMIN", "1234")
+    assert r["success"] == False
+
+def test_exploratoria_caracteres_especiales():
+    r = autenticar_usuario("admin!", "1234")
+    assert r["success"] == False
+
+def test_exploratoria_ambos_campos_vacios():
+    r = autenticar_usuario("", "")
+    assert r["success"] == False
+    assert r["message"] == "Usuario y contraseña son requeridos"
+
+def test_exploratoria_password_con_espacios():
+    r = autenticar_usuario("admin", " 1234 ")
+    assert r["success"] == False
